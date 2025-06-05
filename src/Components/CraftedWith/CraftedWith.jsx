ee463801da505a5e20247d79e70578d80b80730b
Naveennuo <naveenkumarks27@gmail.com>
2025-06-05 17:51:41 +0530

    Update CraftedWith.jsx

--- src/Components/CraftedWith/CraftedWith.jsx
+++ src/Components/CraftedWith/CraftedWith.jsx
@@ -4,7 +4,7 @@ import './CraftedWith.css';
 const CraftedWith = () => {
   return (
     <section className="crafted-with">
-      <h1 className="crafted-with-heading">Crafted , Delivered with Care.</h1>
+      <h1 className="crafted-with-heading">Crafted with Heart, Delivered with Care.</h1>
       <p className="crafted-with-subheading">
   At CraftzCart, we believe every handmade product tells a story. We’re on a mission to bring authentic, handcrafted goods to your doorstep—while supporting local artisans and traditional crafts.
   <br /><br />
